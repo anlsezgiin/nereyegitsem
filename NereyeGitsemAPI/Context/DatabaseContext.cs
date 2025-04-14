daef9c988ef79170ae657f84a90d6469071556f9
Nazar <nazargoksal@icloud.com>
2025-04-14 20:55:32 +0300

    favourites-profile-signup-sigin-endpointler

---
+++ NereyeGitsemAPI/Context/DatabaseContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace NereyeGitsemAPI.Context;
+
+public partial class DatabaseContext : DbContext
+{
+    public DatabaseContext()
+    {
+    }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Favourite> Favourites { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=192.168.1.103;Database=nereyeGitsem;User Id=myuser;Password=bitirmeprojesi123;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Favourite>(entity =>
+        {
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("User");
+
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.Name).HasMaxLength(256);
+            entity.Property(e => e.NewPassword).HasMaxLength(128);
+            entity.Property(e => e.Password).HasMaxLength(128);
+            entity.Property(e => e.Phone).HasMaxLength(64);
+            entity.Property(e => e.Surname).HasMaxLength(256);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
